0166a5c112fdc2e5c429dc6f7c8eda8a5d485b3e
enkhjino <enkhjin.ochirbal1@gmail.com>
2022-08-01 15:12:05 -0700

    2pointers way

--- problems.js
+++ problems.js
@@ -11,4 +11,27 @@ function twoSum(nums, target){
     }
 }
 
-console.log(twoSum([2,7,11,15,3], 5));
+//console.log(twoSum([2,7,11,15,3], 5));
+
+var nums = [2,7,11,15,3];
+var target = 5;
+function twoSum1(nums, target){
+    var sorted = nums.sort((a,b) => a-b);
+    let left = 0;
+    let right = sorted.length - 1;
+    while (left < right) {
+      let add = sorted[left] + sorted[right];
+      console.log(add, "add");
+      if (add === target) {
+        return [left, right];
+      } else if (add > target) {
+        right--;
+        console.log(right, "right");
+      } else {
+        left++;
+        console.log(left, "left");
+      }
+    }
+}
+
+twoSum1()
